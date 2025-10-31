7f2d9e854497f33212f5ee3eb42cab423cfb7256
Matheus Tresguerras <97244120+Revoltzin@users.noreply.github.com>
2025-10-31 21:04:33 +0000

    Created the main files, The repository and Controller structure

---
+++ src/Repository/ContactsRepository.js
@@ -0,0 +1,18 @@
+const { uuid } = require("uuidv4");
+
+let contacts = [
+  {
+    id: uuid(),
+    name: "Matheus",
+    email: "matheus@gmail.com",
+    phone: "2313133",
+    category: uuid(),
+  },
+  {
+    id: uuid(),
+    name: "Josep",
+    email: "josep@gmail.com",
+    phone: "345525",
+    category: uuid(),
+  },
+];
